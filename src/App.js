5365b86c6f2a3448acd829d40065b83f800edb82
avicrayyy <dsgbdomingo@gmail.com>
2021-05-04 22:03:49 +0800

    Added React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 // components
@@ -9,10 +10,12 @@ import Watchlist from "./pages/Watchlist/Watchlist";
 
 function App() {
   return (
-    <div className="coin-app">
-      <Navbar />
-      <Watchlist />
-    </div>
+    <Router>
+      <div className="coin-app">
+        <Navbar />
+        <Watchlist />
+      </div>
+    </Router>
   );
 }
 
